336196c8d5f8a9977ed7712b1ae0814680eddff4
Paul <129361725+pvilcapazab@users.noreply.github.com>
2024-03-12 04:39:48 +0000

    Solución al Problema_2

---
+++ Problema_2.py
@@ -0,0 +1,7 @@
+#Bucle para imprimir los ateriscos crecientes
+for i in range(1, 6):
+    print("*" * i)
+
+#Bucle para imprimir los ateriscos decrecientes
+for i in range(4, 0, -1):
+    print("*" * i)
